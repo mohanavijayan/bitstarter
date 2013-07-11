daa6b08d57b03f9c255206861c9e3f26517af5ff
Mohana Vijayan <vmohanabe@gmail.com>
2013-07-11 00:12:28 +0000

    Updated Web.js with File Read

--- web.js
+++ web.js
@@ -1,20 +1,26 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs=require('fs');
+var buf =new Buffer(256);
 app.get('/', function(request, response)
  {
   //response.send('Hello World2!');
-  ReadFiles();
+//  ReadFiles(response);
+
+
+var len=buf.write(fs.readFileSync('index.html','utf8'),'utf-8');
+response.send(buf.toString('utf8',0,len));
+
+
+
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-ReadFiles();
+//ReadFiles();
 });
-
-var fs = require('fs');
 
 /*fs.readFile('index.html','utf8', function (err,data) {
   if (err) {
@@ -22,9 +28,5 @@ var fs = require('fs');
   }
   console.log(data);
 });*/
-var buf =new Buffer(256);
-function ReadFiles()
-{
-var len=buf.write(fs.readFileSync('index.html','utf8'),'utf-8');
-response.send(buf.toString('utf8',0,len));
-}
+
+
