e5354367ff7f9a4e2e12bd84bb0ca9e92602db5b
Ubuntu <ubuntu@ip-172-31-42-98.us-west-2.compute.internal>
2013-07-22 19:31:35 +0000

    Changes Done

--- web.js
+++ web.js
@@ -2,35 +2,32 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs=require('fs');
-var buf =new Buffer(10240);
+var htmlfile="index.html";
+
+//var buf =new Buffer(10240);
 app.get('/', function(request, response)
  {
   //response.send('Hello World2!');
 //  ReadFiles(response);
 
 
-var len=buf.write(fs.readFileSync('index.html','utf8'),'utf-8');
-response.send(buf.toString('utf8',0,len));
+//var len=buf.write(fs.readFileSync('index.html','utf8'),'utf-8');
+//response.send(buf.toString('utf8',0,len));
 
-
+var html=fs.readFileSync(htmlfile).toString();
+response.send(html);
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 //ReadFiles();
 });
 
-/*fs.readFile('index.html','utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});*/
-
 
 //buf.write(fs.readFileSync('index.html','utf8'),'utf-8');
 //console.log(buf.toString('utf8',0));
-var len=buf.write(fs.readFileSync('index.html','utf8'),'utf-8');
-console.log(buf.toString('utf8',0,len));
+//var len=buf.write(fs.readFileSync('index.html','utf8'),'utf-8');
+//console.log(buf.toString('utf8',0,len));
+
